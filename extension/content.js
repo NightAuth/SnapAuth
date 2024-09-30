6c1a40a7150c4a8c712ae0da3a460955c2fca7fd
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-09-30 23:24:54 +0530

    Remove Extension Folder

--- extension/content.js
+++
@@ -1,6 +0,0 @@
-
-fetch('http://localhost:5000/video_feed')
-  .then(response => {
-    // handle video stream in extension
-  })
-  .catch(error => console.log('Error fetching video feed:', error));
